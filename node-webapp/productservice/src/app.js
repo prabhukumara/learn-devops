9c8728460cb63d648b43bd0f3e9c1c6aad455191
prabhukumara <36650533+prabhukumara@users.noreply.github.com>
2021-12-13 16:56:30 +0530

    Update app.js

--- node-webapp/productservice/src/app.js
+++ node-webapp/productservice/src/app.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(9100);
 console.log('Server has started');
 
 function onRequest(request, response){
